bb3d68a3fbc3700038ad99cbb081183f97fb5f43
HUAI SHUO <huayshuo@gmail.com>
2021-04-06 16:49:20 +0800

    4-06

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 stories_path = './data'
 tokenized_path = './data_token'
+
+cnn_tokenized_path = './data_all_token/cnn_stories_tokenized'
+dm_tokenized_path = './data_all_token/dm_stories_tokenized'
