3cd92f91a2657c3d2f621f993615cac4acb02640
HUAI SHUO <huayshuo@gmail.com>
2021-04-07 18:01:22 +0800

    4-07

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ import torch
 def to_cuda(tensor):
     # turns to cuda
     if torch.cuda.is_available():
-        # return tensor.cuda()
-        return tensor
+        return tensor.cuda()
+        # return tensor
     else:
         return tensor
