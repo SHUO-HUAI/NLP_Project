0d69be518e78586823f198945e0aa3665ede5d95
Huai Shuo <54792401+SHUO-HUAI@users.noreply.github.com>
2021-04-12 23:15:57 +0800

    c

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ tokenized_path = './data_token'
 
 all_tokenized_path = './data_all_token/'
 split_file = {'test': './test_all.txt', 'train': './train_all.txt', 'val': './val_all.txt'}
-load_data = './data_finished_50k'
+load_data = './data_finished_50k_tiny'
 resume = None
-epoch = 30
+epoch = 25
 save = './logs-checkpoint-save'
-batch_size = 128
+batch_size = 100
